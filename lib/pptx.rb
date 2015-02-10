e542256c7f32dd8938b489a82cee24499da7dff6
Michael Frister <michael+git@frister.net>
2015-02-10 17:27:23 +0100

    Implement adding an existing slide
    Still an ugly script.

--- lib/pptx.rb
+++ lib/pptx.rb
@@ -1,8 +1,21 @@
 require 'bundler'
-Bundler.require(:default, 'development')
 
+# Bundler setup instead of require and doing manual requires significantly speeds up startup
+# as not the whole project's dependencies are loaded.
+Bundler.setup(:default, 'development')
+
+require 'nokogiri'
+require 'zip'
+require 'pp'
+require 'securerandom'
+
+# Assumptions
+# * XML namespace prefixes remain the same as in template
+#
 module PPTX
   module OPC
+    RELATIONSHIP_NS = 'http://schemas.openxmlformats.org/package/2006/relationships'
+    CONTENTTYPE_NS = 'http://schemas.openxmlformats.org/package/2006/content-types'
     # Package allows the following:
     # * Create new package based on an existing PPTX file
     #   (extracted ZIP for better version control)
@@ -14,12 +27,30 @@ module PPTX
         @base = File.absolute_path(base)
       end
 
+      def base
+        @base
+      end
+
+      def has_part?(name)
+        parts.has_key?(name)
+      end
+
       def part(name)
         parts[name] ||= IO.read(File.join(@base, name))
       end
 
+      def part_xml(name)
+        Nokogiri::XML(part(name))
+      end
+
       def parts
         @parts ||= Hash[default_part_names.map {|fn| [fn, nil]}]
+      end
+
+      def default_directories
+        @default_directories ||= Dir.glob(File.join(@base, '**/*'))
+                                     .keep_if {|fn| File.directory?(fn)}
+                                     .map     {|fn| fn[(@base.length)..-1][1..-1]}
       end
 
       def default_part_names
@@ -29,13 +60,38 @@ module PPTX
                                    .map    {|fn| fn[(@base.length + 1)..-1]}
       end
 
+      def save
+        parts['[Content_Types].xml'] = @content_type_doc.to_s
+      end
+
+      def set_content_type(part_name, value)
+        @content_type_doc ||= part_xml('[Content_Types].xml')
+        type_list = @content_type_doc.xpath('c:Types', c: CONTENTTYPE_NS).first
+        override = type_list.xpath("./xmlns:Override[@PartName='#{part_name}']").first
+        unless override
+          override = Nokogiri::XML::Node.new('Override', @content_type_doc)
+          override['PartName'] = '/' + part_name
+          type_list.add_child(override)
+        end
+        override['ContentType'] = value
+      end
+
       def to_zip
         buffer = ::StringIO.new('')
+
+
+        entries = parts.map { |n, c| [n, c] } + default_directories.map {|d| [d + '/', nil]}
+        sorted_entries = entries.sort
+
         Zip::OutputStream.write_buffer(buffer) do |out|
-          parts.each do |name, content|
-            content ||= part(name)
-            out.put_next_entry name
-            out.write content
+          sorted_entries.each do |name, content|
+
+            # FIXME
+            unless name.end_with? '/'
+              out.put_next_entry name
+              content ||= part(name)
+              out.write content
+            end
           end
         end
         buffer.string
@@ -43,9 +99,121 @@ module PPTX
     end
 
     class Part
+      def initialize(package, part_name)
+        @package = package
+        @part_name = part_name
+      end
+
+      def content_type
+        nil
+      end
+
+      def doc
+        @doc ||= @package.part_xml(@part_name)
+      end
+
+      def part_name
+        @part_name
+      end
+
+      def relative_part_name(name)
+        name[File.dirname(part_name).size + 1..-1]
+      end
+
+      def relationship_doc
+        @relationship_doc ||= @package.part_xml(relationship_file)
+      end
+
+      def relationship_file
+        File.join(File.dirname(@part_name), '_rels', File.basename(@part_name) + '.rels')
+      end
+
+      def save
+        @package.parts[@part_name] = doc.to_s
+        @package.parts[relationship_file] = relationship_doc.to_s
+        if content_type
+          @package.set_content_type(part_name, content_type)
+        end
+      end
+    end
+  end
+
+  RELATIONSHIP_NS = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships'
+  PRESENTATION_NS = 'http://schemas.openxmlformats.org/presentationml/2006/main'
+  RELTYPE_SLIDE = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/slide'
+
+  class Presentation < OPC::Part
+    def initialize(package, part_name)
+      super(package, part_name)
     end
 
-    class Relationship
+    def add_slide(slide)
+      slide_list = slide_list_xml
+      ref_id = 'rId8'
+      # slide_part_name = 'slides/slide1.xml'
+
+      # TODO remove me - remove existing slide from template
+      # slide_list.xpath('./p:sldId').each do |slide|
+      #   slide_list.children.delete(slide)
+      # end
+
+      # add slide to sldIdList in presentation
+      slide_id = Nokogiri::XML::Node.new('p:sldId', doc)
+      slide_id['id'] = next_slide_id
+      slide_id['r:id'] = ref_id
+      slide_list.add_child(slide_id)
+
+      # add slide to presentation relationships, but first clear existing slides
+      relationship_list = relationship_doc.xpath('r:Relationships', r: OPC::RELATIONSHIP_NS).first
+
+      # TODO remove me - remove existing slide from template
+      # relationship_list.xpath("./xmlns:Relationship[@Type='#{RELTYPE_SLIDE}']").each do |r|
+      #   relationship_list.children.delete(r)
+      # end
+      relationship = Nokogiri::XML::Node.new('Relationship', doc)
+      relationship['Id'] = ref_id
+
+      relationship['Target'] = relative_part_name(slide.part_name)
+      relationship['Type'] = RELTYPE_SLIDE
+      relationship_list.add_child(relationship)
+    end
+
+    def next_slide_id
+      ids = slide_list_xml.xpath('./p:sldId').map { |sid| sid['id'].to_i }
+      return ids.max + 1
+    end
+
+    def slide_list_xml
+      doc.xpath('/p:presentation/p:sldIdLst', p: PRESENTATION_NS).first
+    end
+  end
+
+  class Slide < OPC::Part
+    def initialize(package, part_name)
+      @package = package
+      @part_name = part_name
+    end
+
+    def content_type
+      'application/vnd.openxmlformats-officedocument.presentationml.slide+xml'
+    end
+
+    def doc
+      @doc ||= default_doc @part_name, 'ppt/slides/slide1.xml'
+    end
+
+    def relationship_doc
+      @relationship_doc ||= default_doc relationship_file, 'ppt/slides/_rels/slide1.xml.rels'
+    end
+
+    # Simplification for not wanting to fully generate all XML documents, but rather
+    # load a template and modify it.
+    def default_doc(part_name, default_file)
+      if @package.has_part?(part_name)
+        super
+      else
+        Nokogiri::XML(IO.read(File.join(@package.base, default_file)))
+      end
     end
   end
 end
@@ -55,11 +223,20 @@ end
 def main
   pkg = PPTX::OPC::Package.new
 
-  app = Nokogiri::XML(pkg.part 'docProps/app.xml')
-  slides = app.xpath('/ep:Properties/ep:Slides',
-    ep: 'http://schemas.openxmlformats.org/officeDocument/2006/extended-properties').first
-  slides.content = 1
-  pkg.parts['docProps/app.xml'] = app.to_s
+  # TODO move this to presentation
+  # app = pkg.part_xml 'docProps/app.xml'
+  # slides = app.xpath('/ep:Properties/ep:Slides',
+  #   ep: 'http://schemas.openxmlformats.org/officeDocument/2006/extended-properties').first
+  # slides.content = 1
+  # pkg.parts['docProps/app.xml'] = app.to_s
+
+  slide = PPTX::Slide.new(pkg, 'ppt/slides/slide2.xml')
+  slide.save
+
+  presentation = PPTX::Presentation.new(pkg, 'ppt/presentation.xml')
+  presentation.add_slide(slide)
+  presentation.save
+  pkg.save
 
   pp pkg.parts
 
