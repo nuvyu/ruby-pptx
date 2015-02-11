0fbcbf60e2969b53d2635133b0a60deb3ba006b6
Michael Frister <michael+git@frister.net>
2015-02-11 09:49:51 +0100

    Refactor: Package knows part objects
    Parts register themselves with a given package on creation.
    This allows the package to ask the parts to marshal themselves,
    compared to manually calling `save` on each part.
    
    Part handling in package still is a bit ugly as it has to handle
    them differently depending on whether it knows them as an object,
    a string or hasn't loaded their contents at all.

--- lib/pptx.rb
+++ lib/pptx.rb
@@ -11,7 +11,9 @@ require 'securerandom'
 
 # Assumptions
 # * XML namespace prefixes remain the same as in template
-#
+# * Main presentation is in ppt/presentation.xml
+# * Base presentation is trusted. Nothing is done to prevent directory traversal.
+# If you wanted to open arbitrary presentation, you'd have to make sure to no longer make these.
 module PPTX
   module OPC
     RELATIONSHIP_NS = 'http://schemas.openxmlformats.org/package/2006/relationships'
@@ -31,16 +33,57 @@ module PPTX
         @base
       end
 
+      def content_types
+        @content_types = ContentTypes.new(self, '[Content_Types].xml')
+      end
+
       def has_part?(name)
         parts.has_key?(name)
+      end
+
+      def base_part_xml(name)
+        Nokogiri::XML(IO.read(File.join(@base, name)))
       end
 
       def part(name)
         parts[name] ||= IO.read(File.join(@base, name))
       end
 
+      def marshal_part(name)
+        unless has_part? name
+          raise "marhsal_paprt: unknown part: #{name}"
+        end
+
+        part = parts[name]
+        if part
+          if part.is_a? BasePart
+            part.marshal
+          elsif part.instance_of? String
+            part
+          else
+            raise "marshal_part: Unknown part type: '#{p.class}' for part '#{name}'"
+          end
+        else
+          IO.read(File.join(@base, name))
+        end
+      end
+
+      def set_part(name, obj, content_type)
+        parts[name] = obj
+        if content_type
+          content_types.set_override(name, content_type)
+        end
+      end
+
       def part_xml(name)
-        Nokogiri::XML(part(name))
+        p = part(name)
+        if p.is_a? BasePart
+          p.doc
+        elsif p.instance_of? String
+          Nokogiri::XML(p)
+        else
+          raise "part_xml: Unknown part type: '#{p.class}' for part '#{name}'"
+        end
       end
 
       def parts
@@ -54,20 +97,8 @@ module PPTX
                                    .map    {|fn| fn[(@base.length + 1)..-1]}
       end
 
-      def save
-        parts['[Content_Types].xml'] = @content_type_doc.to_s
-      end
-
-      def set_content_type(part_name, value)
-        @content_type_doc ||= part_xml('[Content_Types].xml')
-        type_list = @content_type_doc.xpath('c:Types', c: CONTENTTYPE_NS).first
-        override = type_list.xpath("./xmlns:Override[@PartName='#{part_name}']").first
-        unless override
-          override = Nokogiri::XML::Node.new('Override', @content_type_doc)
-          override['PartName'] = '/' + part_name
-          type_list.add_child(override)
-        end
-        override['ContentType'] = value
+      def presentation
+        @presentation ||= PPTX::Presentation.new(self, 'ppt/presentation.xml')
       end
 
       def to_zip
@@ -77,7 +108,7 @@ module PPTX
           parts.each do |name, content|
             out.put_next_entry name
             content ||= part(name)
-            out.write content
+            out.write marshal_part(name)
           end
         end
 
@@ -85,10 +116,19 @@ module PPTX
       end
     end
 
-    class Part
-      def initialize(package, part_name)
+    # A part without relationships.
+    # * template_part allows defining a part name that can be used as initial XML document
+    #   when a part with the name doesn't exist yet, but the part shouldn't be created
+    #   with an empty XML document (e.g. when the creation of the whole document isn't
+    #   implemented, but only certain operations).
+    class BasePart
+      def initialize(package, part_name, template_part = nil)
         @package = package
         @part_name = part_name
+        if template_part
+          @doc = @package.part_xml(template_part)
+        end
+        package.set_part(part_name, self, content_type)
       end
 
       def content_type
@@ -96,31 +136,53 @@ module PPTX
       end
 
       def doc
-        @doc ||= @package.part_xml(@part_name)
+        @doc ||= @package.base_part_xml(@part_name)
+      end
+
+      def marshal
+        doc.to_s
       end
 
       def part_name
         @part_name
+      end
+    end
+
+    class Part < BasePart
+      def initialize(package, part_name, template_part = nil)
+        super(package, part_name, template_part)
+        rel_template_part = template_part ? relationship_file(template_part) : nil
+        @relationships = Relationships.new(package, relationship_file, rel_template_part)
       end
 
       def relative_part_name(name)
         name[File.dirname(part_name).size + 1..-1]
       end
 
-      def relationship_doc
-        @relationship_doc ||= @package.part_xml(relationship_file)
+      def relationships
+        @relationships
       end
 
-      def relationship_file
-        File.join(File.dirname(@part_name), '_rels', File.basename(@part_name) + '.rels')
+      def relationship_file(part_name = nil)
+        part_name ||= @part_name
+        File.join(File.dirname(part_name), '_rels', File.basename(part_name) + '.rels')
       end
+    end
 
-      def save
-        @package.parts[@part_name] = doc.to_s
-        @package.parts[relationship_file] = relationship_doc.to_s
-        if content_type
-          @package.set_content_type(part_name, content_type)
+    class Relationships < BasePart
+
+    end
+
+    class ContentTypes < BasePart
+      def set_override(part_name, value)
+        type_list = doc.xpath('c:Types', c: CONTENTTYPE_NS).first
+        override = type_list.xpath("./xmlns:Override[@PartName='#{part_name}']").first
+        unless override
+          override = Nokogiri::XML::Node.new('Override', doc)
+          override['PartName'] = '/' + part_name
+          type_list.add_child(override)
         end
+        override['ContentType'] = value
       end
     end
   end
@@ -151,7 +213,7 @@ module PPTX
       slide_list.add_child(slide_id)
 
       # add slide to presentation relationships, but first clear existing slides
-      relationship_list = relationship_doc.xpath('r:Relationships', r: OPC::RELATIONSHIP_NS).first
+      relationship_list = relationships.doc.xpath('r:Relationships', r: OPC::RELATIONSHIP_NS).first
 
       # TODO remove me - remove existing slide from template
       # relationship_list.xpath("./xmlns:Relationship[@Type='#{RELTYPE_SLIDE}']").each do |r|
@@ -177,30 +239,11 @@ module PPTX
 
   class Slide < OPC::Part
     def initialize(package, part_name)
-      @package = package
-      @part_name = part_name
+      super(package, part_name, 'ppt/slides/slide1.xml')
     end
 
     def content_type
       'application/vnd.openxmlformats-officedocument.presentationml.slide+xml'
-    end
-
-    def doc
-      @doc ||= default_doc @part_name, 'ppt/slides/slide1.xml'
-    end
-
-    def relationship_doc
-      @relationship_doc ||= default_doc relationship_file, 'ppt/slides/_rels/slide1.xml.rels'
-    end
-
-    # Simplification for not wanting to fully generate all XML documents, but rather
-    # load a template and modify it.
-    def default_doc(part_name, default_file)
-      if @package.has_part?(part_name)
-        super
-      else
-        Nokogiri::XML(IO.read(File.join(@package.base, default_file)))
-      end
     end
   end
 end
@@ -218,14 +261,9 @@ def main
   # pkg.parts['docProps/app.xml'] = app.to_s
 
   slide = PPTX::Slide.new(pkg, 'ppt/slides/slide2.xml')
-  slide.save
 
-  presentation = PPTX::Presentation.new(pkg, 'ppt/presentation.xml')
+  presentation = pkg.presentation
   presentation.add_slide(slide)
-  presentation.save
-  pkg.save
-
-  pp pkg.parts
 
   File.open('tmp/generated.pptx', 'wb') {|f| f.write(pkg.to_zip) }
 end
