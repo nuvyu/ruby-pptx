0a5da008e47285efcdf9107b51e0c9172486ab12
Michael Frister <michael+git@frister.net>
2015-02-12 12:53:47 +0100

    Split up opc/other.rb

--- lib/pptx/opc.rb
+++ lib/pptx/opc.rb
@@ -1,3 +1,4 @@
 require_relative 'opc/package'
 require_relative 'opc/part'
-require_relative 'opc/other'
+require_relative 'opc/content_types'
+require_relative 'opc/relationships'
--- lib/pptx/opc/relationships.rb
+++ lib/pptx/opc/relationships.rb
@@ -2,22 +2,6 @@ require 'securerandom'
 
 module PPTX
   module OPC
-
-    class ContentTypes < BasePart
-      NS = 'http://schemas.openxmlformats.org/package/2006/content-types'
-
-      def set_override(part_name, value)
-        type_list = doc.xpath('c:Types', c: NS).first
-        override = type_list.xpath("./xmlns:Override[@PartName='#{part_name}']").first
-        unless override
-          override = Nokogiri::XML::Node.new('Override', doc)
-          override['PartName'] = '/' + part_name
-          type_list.add_child(override)
-        end
-        override['ContentType'] = value
-      end
-    end
-
     class Relationships < BasePart
       NS = 'http://schemas.openxmlformats.org/package/2006/relationships'
 
