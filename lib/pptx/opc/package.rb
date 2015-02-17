67b2b93f7c5d855af05dce7a8a0be4ecb9c1bf04
Michael Frister <michael+git@frister.net>
2015-02-17 12:34:09 +0100

    Begin implementing streaming
    Attachments are still copied to memory, but PPTX package parts are streamed from there.

--- lib/pptx/opc/package.rb
+++ lib/pptx/opc/package.rb
@@ -72,6 +72,43 @@ module PPTX
         @presentation ||= PPTX::Presentation.new(self, 'ppt/presentation.xml')
       end
 
+      class ZipStreamer
+        def initialize(pkg)
+          @pkg = pkg
+        end
+
+        # This method uses parts of zipline to implement streaming.
+        #
+        # This is how Zipline usually works:
+        # * Rails calls response_body.each
+        # * response_body is Zipline::ZipGenerator, it creates a Zipline::FakeStream with the
+        #   block given to each
+        # * The FakeStream is used as output for ZipLine::OutputStream (which inherits
+        #   from Zip::OutputStream)
+        # * ZipGenerator goes through files and calls write_file on the OutputStream for each file
+        # * OutputStream writes the data as String to FakeStream via the << method
+        # * FakeStream yields the String to the block given by Rails
+        #
+        # This method uses Zipline's FakeStream and OutputStream, but writes the entries
+        # to the zip file on it's own, so we can write data for a file in multiple steps
+        # and don't have to rely on Zipline's crazy logic (e.g. on what can be streamed and
+        # what not - only IO and StringIO). Basically, this replaces ZipGenerator.
+        def each(&block)
+          fake_stream = Zipline::FakeStream.new(&block)
+          Zipline::OutputStream.open(fake_stream) do |zip|
+            @pkg.instance_variable_get(:@parts).each do |name, _|
+              data = @pkg.marshal_part(name)
+              zip.put_next_entry name, data.size
+              zip << data
+            end
+          end
+        end
+      end
+
+      def stream_zip
+        return ZipStreamer.new(self)
+      end
+
       def to_zip
         buffer = ::StringIO.new('')
 
