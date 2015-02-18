a5f131313d911282c2dd25e0c2de0058e09caf28
Michael Frister <michael+git@frister.net>
2015-02-18 12:15:09 +0100

    Sample: Add date below title

--- lib/pptx/generate_sample.rb
+++ lib/pptx/generate_sample.rb
@@ -11,23 +11,28 @@ def main
   pkg = PPTX::OPC::Package.new
   slide = PPTX::Slide.new(pkg)
 
-  slide.add_textbox([14*PPTX::CM, 6*PPTX::CM, 10*PPTX::CM, 10*PPTX::CM],
+  title_dimensions = [2*PPTX::CM, 1*PPTX::CM, 22*PPTX::CM, 2*PPTX::CM]
+  date_dimensions = [2*PPTX::CM, 2.8*PPTX::CM, 5*PPTX::CM, 2*PPTX::CM]
+  text_dimensions = [14*PPTX::CM, 6*PPTX::CM, 10*PPTX::CM, 10*PPTX::CM]
+  image_dimensions = [2 * PPTX::CM, 5*PPTX::CM, 10*PPTX::CM, 10*PPTX::CM]
+
+  slide.add_textbox title_dimensions, 'Title :)', sz: 45*PPTX::POINT
+  slide.add_textbox date_dimensions, '18 Feb 2015'
+  slide.add_textbox text_dimensions,
                     "Text box text with long text that should be broken " +
-                    "down into multiple lines.\n:)\nwith<stuff>&to<be>escaped\nYay!")
-  slide.add_textbox([2*PPTX::CM, 1*PPTX::CM, 22*PPTX::CM, 3*PPTX::CM],
-                    'Title :)', sz: 45*PPTX::POINT)
+                    "down into multiple lines.\n:)\nwith<stuff>&to<be>escaped\nYay!"
 
   image = PPTX::OPC::FilePart.new(pkg, 'spec/fixtures/files/test_photo.jpg')
-  slide.add_picture([2 * PPTX::CM, 5*PPTX::CM, 10*PPTX::CM, 10*PPTX::CM],
-                  'photo.jpg', image)
+  slide.add_picture image_dimensions, 'photo.jpg', image
 
   pkg.presentation.add_slide(slide)
 
   slide2 = PPTX::Slide.new(pkg)
-  slide2.add_textbox([2*PPTX::CM, 1*PPTX::CM, 22*PPTX::CM, 3*PPTX::CM],
-                    'Very long title that should be broken up into multiple' +
-                    'lines and possibly clipped', sz: 45*PPTX::POINT)
-  slide2.add_textbox [14*PPTX::CM, 6*PPTX::CM, 10*PPTX::CM, 10*PPTX::CM],
+  slide2.add_textbox title_dimensions,
+                     'Very long title that should be broken up into multiple' +
+                     'lines and possibly clipped', sz: 45*PPTX::POINT
+  slide2.add_textbox date_dimensions, '17 Feb 2015'
+  slide2.add_textbox text_dimensions,
                      "Text box text with long text that should be broken " +
                      "up into multiple lines and should overflow overflow" +
                      " overflowoverflowoverflowoverflowoverflowoverflow" +
